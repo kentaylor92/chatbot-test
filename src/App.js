c4fc3420014200e562dd951d7474dd6cfa705de8
kentaylor92 <ken.taylor.codes@gmail.com>
2021-02-08 12:07:16 -0500

    netlify

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <h1>CHATBOT TEST</h1>
+        <h1>CHATBOT TEST </h1>
         
       </header>
     </div>
